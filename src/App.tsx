27db5deb1a995ddc6e6ed1a1a3b66b17b2593a03
Sujal-781 <159678579+Sujal-781@users.noreply.github.com>
2025-07-09 15:12:37 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,38 @@
-@@ .. @@
- import { Login } from './pages/Login';
- import { Register } from './pages/Register';
- import { Dashboard } from './pages/Dashboard';
- import { Issues } from './pages/Issues';
-+import { Profile } from './pages/Profile';
-+import { MyIssues } from './pages/MyIssues';
- 
- function App() {
-@@ .. @@
-             <Route
-               path="/issues"
-               element={
-                 <ProtectedRoute>
-                   <Navbar />
-                   <Issues />
-                 </ProtectedRoute>
-               }
-             />
-+            <Route
-+              path="/profile"
-+              element={
-+                <ProtectedRoute>
-+                  <Navbar />
-+                  <Profile />
-+                </ProtectedRoute>
-+              }
-+            />
-+            <Route
-+              path="/my-issues"
-+              element={
-+                <ProtectedRoute>
-+                  <Navbar />
-+                  <MyIssues />
-+                </ProtectedRoute>
-+              }
-+            />
-             <Route path="/" element={<Navigate to="/dashboard" replace />} />
-           </Routes>
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { Dashboard } from './pages/Dashboard';
+import { Issues } from './pages/Issues';
+import { Profile } from './pages/Profile';
+import { MyIssues } from './pages/MyIssues';
+
+function App() {
+            <Route
+              path="/issues"
+              element={
+                <ProtectedRoute>
+                  <Navbar />
+                  <Issues />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <Navbar />
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/my-issues"
+              element={
+                <ProtectedRoute>
+                  <Navbar />
+                  <MyIssues />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          </Routes>
+}
